cd2224d70de30049b73c6fa1e8507caa89333d5d
ross.chang <ross.chang@gmail.com>
2017-07-06 15:13:01 +0800

    update

--- scrap.js
+++ scrap.js
@@ -1,6 +1,15 @@
 const phantom = require('phantom');
 const fs = require('fs');
 
+var debug = true;
+var start = process.hrtime();
+var elapsed_time = function(note){
+    var precision = 3; // 3 decimal places
+    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+    start = process.hrtime(); // reset the timer
+}
+
 (async function() {
     const instance = await phantom.create();
     const page = await instance.createPage();
@@ -8,16 +17,21 @@ const fs = require('fs');
     var ti, co;
     var urlPath = 'http://www.166xs.com/xiaoshuo/57/57720/';
     var urlFile = '29090842.html';
+    var outFile;
     var index = 1;
     const regexBr = /<br\s*[\/]?>/gi;
     const regexNbsp = /&nbsp;/g
 
+    // appears to be faster without this event tied up
+    // await page.on("onResourceRequested", function(requestData) {
+    //         console.info('Requesting', requestData.url)
+    //     });
+    
+    //if(debug) elapsed_time("start loop");
+    console.time('scrap');
+
     while(true)
     {
-        await page.on("onResourceRequested", function(requestData) {
-            console.info('Requesting', requestData.url)
-        });
-
         const status = await page.open(urlPath + urlFile);
         console.log(status);
 
@@ -25,27 +39,33 @@ const fs = require('fs');
         await page.injectJs('./jquery-1.10.2.min.js');
 
         //page.evaluateJavaScript('function() { return document.getElementById(\'content\').innerHTML; }')
-        await page.evaluateJavaScript('function() { $(".Book_Text .Book_Hot").remove(); $(".Book_Text script").remove(); $(".Book_Text .Banner").remove(); return {title:$("#content h2").html(), content:$(".Book_Text").html()};}')
+        await page.evaluateJavaScript('function() { $(".Book_Text .Book_Hot").remove(); $(".Book_Text script").remove(); $(".Book_Text .Banner").remove(); return {title:$("#content h2").html(), content:$(".Book_Text").html(), nextHref: $("div.thumb a").filter(function(index) { return $(this).text() === "下一章"; }).attr("href")};}')
         .then(function(html){
             ti = html.title;
             co = html.content.replace(regexBr, "\n").replace(regexNbsp, ' ');
+            outFile = urlFile;
+            urlFile = html.nextHref;
         });
 
-        fs.writeFile('./output/'+ urlFile + '.' + index + '.'+'.txt', ti + co, function(err) {
+        fs.writeFile('./output/'+ outFile + '.' + index + '.txt', ti + co, function(err) {
             if(err) {
                 return console.log(err);
             }
         });
 
-        await page.evaluateJavaScript('function() { return $("div.thumb a").filter(function(index) { return $(this).text() === "下一章"; }).attr("href"); }')
-        .then(function(nextHref){
-            urlFile = nextHref;
-        });
+        // Dont have to evaluate twice, just include the data in JSON returned above.
+        // await page.evaluateJavaScript('function() { return $("div.thumb a").filter(function(index) { return $(this).text() === "下一章"; }).attr("href"); }')
+        // .then(function(nextHref){
+        //     urlFile = nextHref;
+        // });
 
         if (urlFile==='' || urlFile==='index.html') break;
 
         index ++;
     }
+    //if(debug) elapsed_time("end loop");
+    console.timeEnd('scrap');
 
     await instance.exit();
 }());
+
