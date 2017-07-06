da3330c25579c25a2717f5c96f7f1a248735122a
ross.chang <ross.chang@gmail.com>
2017-07-06 17:32:30 +0800

    update

--- scrap.js
+++ scrap.js
@@ -3,14 +3,30 @@ const fs = require('fs');
 
 var debug = true;
 var start = process.hrtime();
-var elapsed_time = function(note){
-    var precision = 3; // 3 decimal places
-    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
-    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
-    start = process.hrtime(); // reset the timer
+// var elapsed_time = function(note){
+//     var precision = 3; // 3 decimal places
+//     var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+//     console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+//     start = process.hrtime(); // reset the timer
+// }
+
+function elapsed_time(note) {
+    return new Promise(function (resolve, reject) {
+        try {
+            var precision = 3; // 3 decimal places
+            var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+            var ou = process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note; // print message + time
+
+            start = process.hrtime(); // reset the timer
+
+            resolve(ou);
+        } catch (err) {
+            reject(new Error(err));
+        }
+    });
 }
 
-(async function() {
+(async function () {
     const instance = await phantom.create();
     const page = await instance.createPage();
 
@@ -26,12 +42,12 @@ var elapsed_time = function(note){
     // await page.on("onResourceRequested", function(requestData) {
     //         console.info('Requesting', requestData.url)
     //     });
-    
-    //if(debug) elapsed_time("start loop");
-    console.time('scrap');
 
-    while(true)
-    {
+    //if(debug) elapsed_time("start loop");
+    //console.time('scrap');
+    if (debug) console.log(await elapsed_time('start '));
+
+    while (true) {
         const status = await page.open(urlPath + urlFile);
         console.log(status);
 
@@ -40,15 +56,15 @@ var elapsed_time = function(note){
 
         //page.evaluateJavaScript('function() { return document.getElementById(\'content\').innerHTML; }')
         await page.evaluateJavaScript('function() { $(".Book_Text .Book_Hot").remove(); $(".Book_Text script").remove(); $(".Book_Text .Banner").remove(); return {title:$("#content h2").html(), content:$(".Book_Text").html(), nextHref: $("div.thumb a").filter(function(index) { return $(this).text() === "下一章"; }).attr("href")};}')
-        .then(function(html){
-            ti = html.title;
-            co = html.content.replace(regexBr, "\n").replace(regexNbsp, ' ');
-            outFile = urlFile;
-            urlFile = html.nextHref;
-        });
+            .then(function (html) {
+                ti = html.title;
+                co = html.content.replace(regexBr, "\n").replace(regexNbsp, ' ');
+                outFile = urlFile;
+                urlFile = html.nextHref;
+            });
 
-        fs.writeFile('./output/'+ outFile + '.' + index + '.txt', ti + co, function(err) {
-            if(err) {
+        fs.writeFile('./output/' + outFile + '.' + index + '.txt', ti + co, function (err) {
+            if (err) {
                 return console.log(err);
             }
         });
@@ -59,13 +75,13 @@ var elapsed_time = function(note){
         //     urlFile = nextHref;
         // });
 
-        if (urlFile==='' || urlFile==='index.html') break;
+        if (urlFile === '' || urlFile === 'index.html') break;
 
-        index ++;
+        index++;
     }
     //if(debug) elapsed_time("end loop");
-    console.timeEnd('scrap');
+    //console.timeEnd('scrap');
+    if (debug) console.log(await elapsed_time('end '));
 
     await instance.exit();
 }());
-
